9bfe709cd556e0ac910408feb742d8d0620344df
Emika Ijuin <emikaijuin@gmail.com>
2019-01-03 14:14:14 +0800

    Regenerated schema after migrating groups and light-groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_033256) do
+ActiveRecord::Schema.define(version: 2019_01_03_041059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "light_groups", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id"
+    t.bigint "light_id"
   end
 
   create_table "lights", force: :cascade do |t|
