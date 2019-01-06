a96fb1403c862b1f3d902da5b0eb98d5808cf620
Emika Ijuin <emikaijuin@gmail.com>
2019-01-06 18:59:57 +0800

    Check if light is already saved in database, if not, add to the 'all' group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,9 @@
 class Group < ApplicationRecord
   has_many :light_groups
   has_many :lights, through: :light_groups
+
+  def self.add_light_to_all_lights_group(light)
+    group = Group.find_by(name: "All")
+    group.lights << light if !group.lights.include?(light)
+  end
 end
