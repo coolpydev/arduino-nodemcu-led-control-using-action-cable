e83ff80c645543a6b87adb4d37636b8fda4ddba3
Emika Ijuin <emikaijuin@gmail.com>
2019-01-06 18:59:34 +0800

    Add all connecting lights to the 'all' group

--- app/channels/arduino_channel.rb
+++ app/channels/arduino_channel.rb
@@ -4,6 +4,7 @@ class ArduinoChannel < ApplicationCable::Channel
   
   def identify_device(data)
     @light = Light.find_or_create_by(fingerprint: data.dig("mac"))
+    Group.add_light_to_all_lights_group(@light)
     # Control via individual light channel
     stream_from "arduino_#{@light.fingerprint}"
     ActionCable.server.broadcast("arduino_#{@light.fingerprint}", message: "You're all plugged in, Ada!")
