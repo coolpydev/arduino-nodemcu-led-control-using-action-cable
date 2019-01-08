1b16b07e63f60404f98b1e1729c48e95cbcd0b16
Emika Ijuin <emikaijuin@gmail.com>
2019-01-08 16:51:27 +0800

    Create users and add clearance gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_083944) do
+ActiveRecord::Schema.define(version: 2019_01_08_084508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2019_01_08_083944) do
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
