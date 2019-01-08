a5342aceeef251bb80bd79560bf136f866264c5e
Emika Ijuin <emikaijuin@gmail.com>
2019-01-08 16:02:39 +0800

    Highlight correct button on page load depending on saved group color state

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,24 +1,62 @@
 $(document).on('turbolinks:load', function(){
-  let hexField = document.getElementById('group_hex');
-  let rgbField = document.getElementById('rgb');
+  // let hexField = document.getElementById('group_hex');
+  // let rgbField = document.getElementById('rgb');
 
-  if (hexField) { 
-    hexField.oninput = function(event) {
-      let hex = hexField.value;
-      let rgb = hexToRgb(hex);
-      group_rgb_r.value = rgb.r;
-      group_rgb_g.value = rgb.g;
-      group_rgb_b.value = rgb.b;
-    }
+  // if (hexField) { 
+  //   hexField.oninput = function(event) {
+  //     let hex = hexField.value;
+  //     let rgb = hexToRgb(hex);
+  //     group_rgb_r.value = rgb.r;
+  //     group_rgb_g.value = rgb.g;
+  //     group_rgb_b.value = rgb.b;
+  //   }
+  // }
+
+  // function hexToRgb(hex) {
+  //   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  //   return result ? {
+  //     r: parseInt(result[1], 16),
+  //     g: parseInt(result[2], 16),
+  //     b: parseInt(result[3], 16)
+  //   } : null;
+  // }
+
+  // Set initial button colors
+  let buttonGroups = $('.group-color');
+  Array.from(buttonGroups).forEach(function(buttonGroup){
+    getRgbFromServer(buttonGroup.id);
+  });
+
+  function getRgbFromServer(id) {
+    $.ajaxSetup({
+      headers: { 'X-CSRF-Token': $('#authenticity-token').attr('content') }
+    });
+    $.ajax({
+      method: "POST",
+      url: `/groups/${id}/get_current_state`,
+      dataType: "JSON",
+      success: function(data) {
+        highlightActive(id, data)
+      },
+      error: function(error) {
+        console.log("Something went wrong :( Check server logs.")
+      }
+    }); 
   }
 
-  function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
+  function highlightActive(id, rgb) {
+    let className, rgba;
+    if (rgb.r == "255" && rgb.r == rgb.g && rgb.r == rgb.b) {
+      className = "on";
+      rgb = "255, 255, 255"
+    } else if (rgb.r == "0" && rgb.r == rgb.g && rgb.r == rgb.b ) {
+      className = "off";
+      rgb = "0, 0, 0"     
+    } else {
+      className = "color";
+      rgb = `${rgb.r}, ${rgb.g}, ${rgb.b}`
+    }
+    $(`#${id} > .${className}`).css("background-color", `rgba(${rgb},0.6)`)
   }
   
   // Group Control
@@ -47,21 +85,25 @@ $(document).on('turbolinks:load', function(){
   function sendRgbToServer(rgb, element) {
     $.ajaxSetup({
       headers: { 'X-CSRF-Token': $('#authenticity-token').attr('content') }
-    })
+    });
     $.ajax({
       method: "PUT",
       url: `/groups/${element.parentElement.id}`,
       data: {group: {rgb: rgb}},
       dataType: "JSON",
       success: function(data) {
-        Array.from(element.parentElement.children).forEach(function(el){
-          el.style.backgroundColor = "lightgray";
-          el.style.color = "black";
-        });
-        element.style.backgroundColor = data["color"];
+        updateButton(data["color"], element);
       }
-    })
+    });
   }
+
+  function updateButton(color, element){
+    Array.from(element.parentElement.children).forEach(function(el){
+      el.style.backgroundColor = "lightgray";
+      el.style.color = "black";
+    });
+    element.style.backgroundColor = color;
+  };
 
 });
 
