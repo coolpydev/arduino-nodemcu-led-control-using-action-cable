17c42b80f7d0e2821007f34cb2830feee9ed87a1
Emika Ijuin <emikaijuin@gmail.com>
2019-01-08 15:24:56 +0800

    regenerate schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_041059) do
+ActiveRecord::Schema.define(version: 2019_01_08_063901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_01_03_041059) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "rgb_state"
   end
 
   create_table "light_groups", force: :cascade do |t|
