d5c3ce57e81b468ae4b80571f7864dff2648abad
Emika Ijuin <emikaijuin@gmail.com>
2019-04-03 12:07:47 +0800

    add light to group

--- app/controllers/lights_controller.rb
+++ app/controllers/lights_controller.rb
@@ -2,7 +2,7 @@ class LightsController < ApplicationController
   before_action :set_light, except: [:index, :new, :create]
 
   def index
-    @lights = Light.all
+    @lights = Light.all.includes(:groups)
   end
 
   def new
@@ -24,10 +24,17 @@ class LightsController < ApplicationController
   def destroy
   end
 
-  private 
+  def add_to_group
+    LightGroup.where(light_id: params[:light_id]).destroy_all # in current version, light can only exist in one group at a time
+
+    light_group = LightGroup.create(light_id: params[:light_id], group_id: params[:group_id])
+
+    redirect_to group_path(params[:group_id])
+  end
+
+  private
 
   def set_light
     @light = Light.find(params[:id])
   end
-
 end
