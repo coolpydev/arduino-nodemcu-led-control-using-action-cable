a1c191e0a357eeb6c6cc9b7509dc8cf5e088f2aa
Emika Ijuin <emikaijuin@gmail.com>
2019-04-03 12:08:48 +0800

    Create route to add light to group

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :lights
   post "/lights/change_color", to: "lights#change_color", as: "change_color"
   post "/groups/:id/get_current_state", to: "groups#get_current_state"
-  
+  post "/lights/:id/add_to_group", to: "lights#add_to_group"
+
   resources :groups
 
   root to: "groups#index"
